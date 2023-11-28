8ad881c91f1e1fd7b7b3110f3560249444117fe7
Yahya <148375737+Y4HY41336@users.noreply.github.com>
2023-11-28 20:31:54 +0400

    Add files via upload

---
+++ Task02!/Task02/CustomList.cs
@@ -0,0 +1,74 @@
+﻿namespace Algorithms
+{
+  
+    public class CustomList<T>
+    {
+        private T[] list;
+        private int count = 0;
+        private int capacity;
+        public int Count { get { return count; } }
+        public int Capacity { get { return capacity; } }
+        public CustomList()
+        {
+            int capacity = 4;
+            this.capacity = capacity;
+            list = new T[capacity];
+        }
+        public CustomList(int capacity)
+        {
+            if (capacity < 1) capacity = 1;
+            this.capacity = capacity;
+            list = new T[capacity];
+        }              
+        public void Add(T newElement)
+        {
+            if (count == capacity)
+            {
+                T[] resized = new T[capacity * 2];
+                for (int i = 0; i < capacity; i++)
+                {
+                    resized[i] = list[i];
+                }
+                list = resized;
+                capacity = capacity * 2;
+            }
+
+            list[count] = newElement;
+            count++;
+        }
+        public T? Find(Predicate<T> match)
+        {
+            for (int i = 0; i < count; i++)
+            {
+                if (match(list[i]))
+                {
+                    return list[i];
+                }
+            }
+            return default;
+        }
+        public CustomList<T> FindAll(Predicate<T> match)
+        {
+            CustomList<T> list = new CustomList<T>();
+            for (int i = 0; i < count; i++)
+            {
+                if (match(this.list[i]))
+                {
+                    list.Add(this.list[i]);
+                }
+            }
+            return list;
+        }
+        public bool Contains(T item)
+        {                 
+            if (count != 0 && Array.IndexOf(list, item) != -1)
+            {
+                return true;
+            }
+            return false;
+        }
+
+
+    }
+}
+
---
+++ Task02!/Task02/Program.cs
@@ -0,0 +1,44 @@
+﻿using Algorithms;
+
+CustomList<int> customList = new CustomList<int>();
+customList.Add(1);
+Console.WriteLine(customList.Capacity);
+Console.WriteLine(customList.Count);
+customList.Add(2);
+Console.WriteLine(customList.Capacity);
+Console.WriteLine(customList.Count);
+customList.Add(3);
+Console.WriteLine(customList.Capacity);
+Console.WriteLine(customList.Count);
+customList.Add(4);
+Console.WriteLine(customList.Capacity);
+Console.WriteLine(customList.Count);
+customList.Add(5);
+Console.WriteLine(customList.Capacity);
+Console.WriteLine(customList.Count);
+customList.Add(6);
+Console.WriteLine(customList.Capacity);
+Console.WriteLine(customList.Count);
+customList.Add(7);
+Console.WriteLine(customList.Capacity);
+Console.WriteLine(customList.Count);
+customList.Add(8);
+Console.WriteLine(customList.Capacity);
+Console.WriteLine(customList.Count);
+customList.Add(9);
+Console.WriteLine(customList.Capacity);
+Console.WriteLine(customList.Count);
+customList.FindAll(x => x % 2 == 0);
+Console.WriteLine(customList.Contains(0));
+
+//for (int i = 0; i < customList.Count; i++)
+//{
+
+//}
+
+
+
+
+List<int> ints = new List<int>();
+
+ints.Exists();
