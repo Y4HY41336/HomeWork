85cb74723cb30c9d7c25ece0e12cebc69f260ddd
Yahya <148375737+Y4HY41336@users.noreply.github.com>
2023-11-29 20:00:21 +0400

    Add files via upload

---
+++ !Task02/Task02/Program.cs
@@ -0,0 +1,63 @@
+﻿using Algorithms;
+
+CustomList<int> customList = new CustomList<int>();
+customList.Add(1);
+//Console.WriteLine(customList.Capacity);
+//Console.WriteLine(customList.Count);
+customList.Add(2);
+//Console.WriteLine(customList.Capacity);
+//Console.WriteLine(customList.Count);
+customList.Add(3);
+//Console.WriteLine(customList.Capacity);
+//Console.WriteLine(customList.Count);
+customList.Add(4);
+//Console.WriteLine(customList.Capacity);
+//Console.WriteLine(customList.Count);
+customList.Add(5);
+//Console.WriteLine(customList.Capacity);
+//Console.WriteLine(customList.Count);
+customList.Add(6);
+//Console.WriteLine(customList.Capacity);
+//Console.WriteLine(customList.Count);
+customList.Add(7);
+//Console.WriteLine(customList.Capacity);
+//Console.WriteLine(customList.Count);
+customList.Add(8);
+//Console.WriteLine(customList.Capacity);
+//Console.WriteLine(customList.Count);
+customList.Add(9);
+//Console.WriteLine(customList.Capacity);
+//Console.WriteLine(customList.Count);
+Console.WriteLine(customList.FindAll(x => x % 2 == 0));
+//Console.WriteLine(customList.Find(x => x % 2 == 0));
+//Console.WriteLine(customList.Contains(4));
+//Console.WriteLine(customList.Remove(5));
+//Console.WriteLine(customList.Exists(x => x==12));
+for (int i = 0; i < customList.Count; i++)
+{
+    Console.WriteLine(customList.list[i]);
+
+}
+//Console.WriteLine(customList.FindAll(x => x == 1));
+
+
+
+List<int> ints = new List<int>();
+//ints.Add(1);
+//ints.Add(2);
+//ints.Add(3);
+//ints.Add(4);
+//ints.Add(5);
+//ints.Add(6);
+//ints.Add(7);
+////ints.Find(x => x == 1);
+//Console.WriteLine(ints.Remove(0));
+////ints.Remove(5);
+//foreach (int x in ints)
+//{
+//    Console.WriteLine(x);
+//}
+//Console.WriteLine(ints.Remove(0));
+//Console.WriteLine(ints.Exists(x => x == 2));
+
+//ints.Exists(x => x == 2);
